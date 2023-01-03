1e18a42f1c4122ea67363e3275bad3bc368f4d16
abozhkov <alexander.bojkov89@gmail.com>
2023-01-03 19:00:54 +0200

    Remove ROOT_API from Dockerfile and add NOTE for the secret env variable we use

--- services/frontend/config/prod.env.js
+++ services/frontend/config/prod.env.js
@@ -1,5 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
+  // NOTE: In minikube this part get's evaluated to the hostname:
+  // "http://dev.k8s/api/" from kubernetes -> frontend-secrets -> ROOT_API
   ROOT_API: JSON.stringify(process.env.ROOT_API)
 }
