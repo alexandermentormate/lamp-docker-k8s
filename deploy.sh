af7c172691071d3370c12582827c176521291414
abozhkov <alexander.bojkov89@gmail.com>
2023-01-05 15:09:58 +0200

    Rearrange the kubernetes yaml files and place an explicit wait for the webserver to wait for the backend and frontend containers; Update the deploy.sh file with the changes

--- deploy.sh
+++ deploy.sh
@@ -5,25 +5,26 @@ NC='\e[0m'
 USAGE="$(basename "$0") [-h] [-d --destroy] [-r --rebuild <image_name>] -- deploy script for the project that builds and runs the project on hostname ${RED}dev.k8s${NC}.
 
 where:
-    -h  show this help text
+    -h, --help  show this help text
     -d, --destroy  destroy the entire project, including the persistent volumes
-    -r, --rebuild, <image_name> [optional]  rebuild the project with optional <image_name> (default: flaskapp:1.0)"
+    -rb, --rebuild-backend, <image_name> [optional]  rebuild the backend service with optional <image_name> (default: flaskapp:1.0)
+    -rf, --rebuild-frontend, <image_name> [optional]  rebuild the backend service with optional <image_name> (default: vueapp:1.0)"
     
 
-if [ "$1" = "-h" ]
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]
     then
         echo "$USAGE"
         exit 0
 fi
 
 
-if [ "$1" = "--destroy" ]
+if [ "$1" = "-d" ] || [ "$1" = "--destroy" ]
   then
 
     echo "${RED}Tearing down the local minikube project${NC}"
 
-    kubectl delete -f kubernetes/8-deployment_frontend.yaml
-    kubectl delete -f kubernetes/7-deployment_webserver.yaml
+    kubectl delete -f kubernetes/8-deployment_webserver.yaml
+    kubectl delete -f kubernetes/7-deployment_frontend.yaml
     kubectl delete -f kubernetes/6-deployment_backend.yaml
     kubectl delete -f kubernetes/5-deployment_database.yaml
     kubectl delete -f kubernetes/4-ingress.yaml
@@ -63,7 +64,7 @@ kubectl apply -f ./kubernetes/4-ingress.yaml
 echo "Creating the ${GREEN}deployment${NC} for the database service"
 kubectl create -f ./kubernetes/5-deployment_database.yaml
 
-if [ "$1" = "-r" ] || [ "$1" = "--rebuild" ]
+if [ "$1" = "-rb" ] || [ "$1" = "--rebuild-backend" ]
   then
 
     if [ $# -eq 2 ]
@@ -73,7 +74,7 @@ if [ "$1" = "-r" ] || [ "$1" = "--rebuild" ]
         image_name="flaskapp:1.0"
     fi
 
-    echo "${GREEN}Building new flask image called $image_name${NC}"
+    echo "${GREEN}Building new backend image called $image_name${NC}"
     docker build -t $image_name ./services/backend/
 fi
 
@@ -83,11 +84,28 @@ kubectl delete -f ./kubernetes/6-deployment_backend.yaml
 kubectl apply -f ./kubernetes/6-deployment_backend.yaml
 
 
-echo "Creating or restarting existing ${GREEN}deployment${NC} for the webserver service"
-kubectl delete -f ./kubernetes/7-deployment_webserver.yaml
-kubectl apply -f ./kubernetes/7-deployment_webserver.yaml
+if [ "$1" = "-rf" ] || [ "$1" = "--rebuild-frontend" ]
+  then
+
+    if [ $# -eq 2 ]
+      then
+        image_name=$2
+      else
+        image_name="vueapp:1.0"
+    fi
+
+    echo "${GREEN}Building new frontend image called $image_name${NC}"
+    docker build -t $image_name ./services/frontend/
+fi
 
 
 echo "Creating or restarting existing ${GREEN}deployment${NC} for the frontend service"
-kubectl delete -f ./kubernetes/8-deployment_frontend.yaml
-kubectl apply -f ./kubernetes/8-deployment_frontend.yaml
+kubectl delete -f ./kubernetes/7-deployment_frontend.yaml
+kubectl apply -f ./kubernetes/7-deployment_frontend.yaml
+
+
+echo "Creating or restarting existing ${GREEN}deployment${NC} for the webserver service"
+kubectl wait --for=condition=ready pod -l app=backend-deployment
+kubectl wait --for=condition=ready pod -l app=frontend-deployment
+kubectl delete -f ./kubernetes/8-deployment_webserver.yaml
+kubectl apply -f ./kubernetes/8-deployment_webserver.yaml
